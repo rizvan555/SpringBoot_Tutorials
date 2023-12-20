344a7db6e317aa299464fa92690527bfb40e594f
Rizvan Karimov <karimovrizvan84@gmail.com>
2023-12-20 15:30:05 +0100

    Dependency Injection with Setters

--- src/main/java/spring_introduction/Person.java
+++ src/main/java/spring_introduction/Person.java
@@ -2,8 +2,31 @@ package spring_introduction;
 
 public class Person {
     private Pet pet;
-    public Person(Pet pet) {
+    private String surname;
+    private int age;
+
+
+    public void setPet(Pet pet) {
+        System.out.println("Class Person: set pet");
         this.pet = pet;
+    }
+
+    //Getters
+    public String getSurname() {
+        return surname;
+    }
+    public int getAge() {
+        return age;
+    }
+
+    //Setters
+    public void setSurname(String surname) {
+        System.out.println("Class Person: set surname");
+        this.surname = surname;
+    }
+    public void setAge(int age) {
+        System.out.println("Class Person: set age");
+        this.age = age;
     }
 
     public void callYourPet(){
--- src/test/java/spring_introduction/Test3.java
+++ src/test/java/spring_introduction/Test3.java
@@ -6,8 +6,15 @@ public class Test3 {
     public static void main(String[] args) {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
+        System.out.println("*++++++++++++++++++++++++++++++++++++++++*");
+
     Person person = context.getBean("myPerson", Person.class);
     person.callYourPet();
+
+        System.out.println("*++++++++++++++++++++++++++++++++++++++++*");
+
+        System.out.println("Surname: " + person.getSurname());
+        System.out.println("Age: " + person.getAge());
 
     context.close();
     }
